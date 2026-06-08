3141a75777fc4524a6b3a55108a724fd7e68ecdb
Girish Lade <girishlade111@gmail.com>
2026-06-08 09:29:05 -0700

    feat: configure Vite build with manual chunk splitting and dependency optimization for PDF libraries

--- ls-pdf/vite.config.js
+++ ls-pdf/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['pdf-lib', 'pdfjs-dist'],
+  },
+  build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-pdflib': ['pdf-lib'],
+          'vendor-pdfjs': ['pdfjs-dist'],
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 })
